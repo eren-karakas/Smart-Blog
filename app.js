41d3d696f4e89fbbe031f9ff5be7b247c94184be
Eren Karakas <erenkarakas.ek@gmail.com>
2022-03-23 19:19:09 +0300

    Add Photo Operation

--- app.js
+++ app.js
@@ -1,30 +1,68 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const Photo = require('./models/Photo');
 
-const app = express()
+const server = '127.0.0.1';
+const port = '3000';
 
-const server = '127.0.0.1'
-const port = '3000'
+// CONNECT DB
+app.listen(port, server, async () => {
+  await mongoose.connect('mongodb://localhost/sb-test-db');
+  console.log(`Server is active ! http://${server}:${port}/`);
+});
 
 // TEMPLATE ENGINE
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // MIDDLEWARES
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
+  });
+});
 
 app.get('/about', (req, res) => {
-    res.render('about')
-})
+  res.render('about');
+});
 
 app.get('/add', (req, res) => {
-    res.render('add')
-})
+  res.render('add');
+});
 
-app.listen(port, server, () => {
-    console.log(`Server is active ! http://${server}:${port}/`);
-})
+app.post('/photos', async (req, res) => {
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,  
+    });
+    res.redirect('/');
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
